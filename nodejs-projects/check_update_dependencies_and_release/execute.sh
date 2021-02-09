8708eee0790850b987c68ad4823dc6718527b694
stfsy <s.pfaffel@gmail.com>
2021-02-09 15:56:54 +0100

    chore: use single script

--- nodejs-projects/check_update_dependencies_and_release/execute.sh
+++ nodejs-projects/check_update_dependencies_and_release/execute.sh
@@ -2,9 +2,17 @@
 
 set -ex
 
-FOLDER='check_update_dependencies_and_release'
+declare -r ORIGIN_DOMAIN_AND_PROTOCOL="https://github.com"
+declare -r ORIGIN_USER="stfsy"
+declare -r REPOSITORIES_FILE="_repositories.txt"
+declare -r repositories="$(cat $REPOSITORIES_FILE)"
 
-./"$FOLDER"/01_checkout.sh
-./"$FOLDER"/02_update-and-tag.sh
-./"$FOLDER"/03_publish.sh
-./"$FOLDER"/04_clean.sh
+for repository in $repositories; do
+    git clone "${ORIGIN_DOMAIN_AND_PROTOCOL}/${ORIGIN_USER}/${repository}"
+    
+    cd $repository 
+        bash -c ./.update-deps-and-tag.sh
+    cd ..
+
+    rm -rf $repository 
+done
