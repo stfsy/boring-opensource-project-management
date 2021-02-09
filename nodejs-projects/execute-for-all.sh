d43822216c33a2b4e2ddd7a2320cccf35b5756a1
stfsy <s.pfaffel@gmail.com>
2021-02-09 15:57:32 +0100

    chore: update with latest script

--- nodejs-projects/execute-for-all.sh
+++ nodejs-projects/execute-for-all.sh
@@ -1,16 +1,37 @@
-#/bin/sh
+#!/bin/sh
 
-set -ex
+set -x
 
-declare -r REMOTE_REPOSITORY_BASE_URL="https://github.com/stfsy"
+declare -r ORIGIN_DOMAIN_AND_PROTOCOL="https://github.com"
+declare -r ORIGIN_USER="stfsy"
 declare -r REPOSITORIES_FILE="_repositories.txt"
 declare -r repositories="$(cat $REPOSITORIES_FILE)"
 
 for repository in $repositories; do
+    git clone "${ORIGIN_DOMAIN_AND_PROTOCOL}/${ORIGIN_USER}/${repository}"
+
     cd $repository
-    npm install -D conventional-changelog-cli
-    git add package*
-    git commit -m "chore: install conventional-changelog-cli"
-    git push origin master
+
+    npm install -D conventional-changelog-cli@2.1.1
+    has_changes=$(git diff --name-only | grep package -c)
+
+    if [[ has_changes -gt "0" ]]; then
+        git add .
+        git commit -m "chore: install newest conventional-changelog-cli"
+        git push origin master
+    fi
+
+    npm run changelog
+    has_changes=$(git diff --name-only | grep package -c)
+
+    if [[ has_changes -gt "0" ]]; then
+        git add .
+        git status
+        git commit -m "chore: generate changelog with new format"
+        git push origin master
+    fi
+
     cd ..
+    rm -rf $repository
+
 done
